aed7f2fef9401058cea73f20e37f699262ae89fd
pancake999999 <164941367+pancake999999@users.noreply.github.com>
2024-05-09 18:56:53 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to BloxyFriendo, Listening to SodaMachine"];
+const statusMessages = ["🤡...", "🤡..", "🤡.", "🤡"];
 
 
 let currentIndex = 0;
