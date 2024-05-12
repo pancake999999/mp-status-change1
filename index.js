a37dbb3d1fcd2ca4a9db680a533207a07227b600
pancake999999 <164941367+pancake999999@users.noreply.github.com>
2024-05-12 14:56:02 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["🤡...", "🤡..", "🤡.", "🤡"];
+const statusMessages = [":clown:...", ":clown:..", ":clown:.", ":clown:"];
 
 
 let currentIndex = 0;
